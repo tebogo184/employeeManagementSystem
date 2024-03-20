7f4b5e93c94cd17cd116c247a22ff30aa162ee8f
tebogo184 <tebogotshukudu184@gmail.com>
2024-03-20 19:23:06 +0200

    Initial commit

---
+++ employeeManagementSystem/src/test/java/System/employeeManagementSystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package System.employeeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
